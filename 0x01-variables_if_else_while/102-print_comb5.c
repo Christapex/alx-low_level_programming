e105650bfc481e49643d64ac0bdceab711ff926c
Christapex <anthonyodinajesus@gmail.com>
2022-07-04 03:33:28 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 			{
 				for (j = 48; j <= 57; j++)
 				{
-					if (((K + j) > (c + i) && k >= c) || c < k)
+					if (((k + j) > (c + i) && k >= c) || c < k)
 					{
 
 						putchar(c);
